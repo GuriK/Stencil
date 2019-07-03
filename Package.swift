2344153a2291246d9ad55049be6e0c5e55076d38
Guram Kajaia <GuriK@users.noreply.github.com>
2019-07-03 21:45:52 +0400

    swift 5

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
   name: "Stencil",
   dependencies: [
-    .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 9),
+    .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
 
     // https://github.com/apple/swift-package-manager/pull/597
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 9),
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
   ]
 )
